c5712a2c9dce203e58b2b14cec5f02bc8f6c0fea
William Andrei Nascimento Silveira <william.silveira@pampa.compasso>
2022-11-07 13:10:29 -0300

    correções na calculadora

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -3,7 +3,7 @@
 
     <!--Resultados da Calculadora-->
     <div class="w-full rounded m-1 p-3 text-right lead font-weight-bold text-white bg-vue-dark">
-      {{ calculatorValue || 0 }}
+      {{ calculatorShowValue || 0 }}
     </div>
       
     <!--Botões da calculadora-->
@@ -31,7 +31,9 @@ export default {
 
   data() {
     return {
-      calculatorValue: '',
+      calculatorShowValue:'',//valor para mostrar
+      calculatorValue: '',// valor para clcular
+      resultFlag:false,//um booleano para indicar se o botão '=' ja foi clicado
       calculatorElements: ['C','/','*','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
       operator: null, 
       previousCalculatorValue: '',
@@ -43,31 +45,49 @@ export default {
 
       /* Adicionar valor */
       if(!isNaN(n) || n=== '.') {
+        // se tu tiver clicado em '=' o proximo numero que tu inserir vai zerar o display da calculadora pra mostrar só aquele numero
+        // ex: se tu fez a conta 8+8  e clica '=' aparece 16 na tela dai em vez de apertar o C pra limpar tu clica em outro numero
+        // como tu relizou a operação de igual ele apaga o 16 e mostra o novo numero que tu inseriu, como acontece em uma calculadora normal
+        if(this.resultFlag){
+          this.calculatorValue = '';
+          this.calculatorShowValue='';
+          this.resultFlag =false;// seta pra falso de novo até tu relizarr outra operação de =
+        }
         this.calculatorValue += n + '';
+        this.calculatorShowValue+= n;
       }
 
       /* Botão C para limpar os valores */
       if( n === 'C') {
         this.calculatorValue = '';
+        this.calculatorShowValue='';
       }
 
       /* Botão de porcentagem */
       if( n === '%') {
         this.calculatorValue = this.calculatorValue / 100 + '';
+        this.calculatorShowValue=this.calculatorValue;// depois de fazer o calculo na linha acima o valor pra mostrar na tela recebe o valor real calculado
       }
 
       /* Botões de comandos soma, subtração, multiplicação e divisão */
       if(['/','*','-','+'].includes(n)) {
+        console.log(n);
+        this.calculatorShowValue = this.calculatorValue + n; // aqui ele agrupa o numero + o operador pra mostrar na tela
         this.operator = n;
         this.previousCalculatorValue = this.calculatorValue;
         this.calculatorValue = '';
+        this.resultFlag=false;
+        // aqui eu deixo false porque caso tu queira fazer uma operação com o resultado de uma operação =, ex: 9+1 = 10 dai quando aparece o 10 tu aperta o botão de + e digita 1
+        // dai vai aparecer no display 10 + 1
+        //se não setasse falso aqui ia aparecer só o numero 1 no display porque eu apago o display quando insiro numero la em cima e o igual ja foi apertado.
       }
 
       if( n === '=') {
         this.calculatorValue = eval (
           this.previousCalculatorValue + this.operator + this.calculatorValue
         );
-
+        this.calculatorShowValue = this.calculatorValue;
+        this.resultFlag = true;//seta flag para true para indicar que houve uma operação de =
         this.previousCalculatorValue = '';
         this.operator = null; 
       }
